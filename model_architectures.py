00753170829da6fdb2da740d304944eb9e05a47b
Tommaso Tedeschi <tedesk95@gmail.com>
2021-02-01 16:21:00 +0100

    Create model_architectures.py

---
+++ model_architectures.py
@@ -0,0 +1,122 @@
+from keras.models import Sequential
+from keras.layers import Input, Dense, Activation, Flatten
+import keras.optimizers
+import keras.initializers
+import tensorflow as tf
+import math
+
+def minimal_dense(input_len, output_activation, nb_actions, lr):
+    model = Sequential()
+    model.add(Dense(16, input_dim=input_len))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(nb_actions))
+    model.add(Activation(output_activation))
+    print(model.summary())
+    #opt = keras.optimizers.Adam(learning_rate=0.00001)
+    opt = keras.optimizers.Adam(learning_rate=lr)
+    model.compile(loss=tf.keras.losses.Huber(), optimizer=opt)
+    #model.compile(optimizer='adam', loss=tf.keras.losses.Huber())
+
+    return model
+
+
+
+
+def dense(input_len, output_activation, nb_actions, lr):
+    model = Sequential()
+    model.add(Dense(16, input_dim=input_len))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(32))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(64))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(128))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(64))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(32))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(nb_actions))
+    model.add(Activation(output_activation))
+    print(model.summary())
+    opt = keras.optimizers.Adam(learning_rate=lr)
+    model.compile(loss=tf.keras.losses.Huber(), optimizer=opt)
+    #model.compile(optimizer='adam', loss=tf.keras.losses.Huber())
+
+    return model
+
+def small_dense(input_len, output_activation, nb_actions, lr, seed_):
+    model = Sequential()
+    initializer = keras.initializers.glorot_uniform(seed = seed_)
+    model.add(Dense(16, input_dim=input_len, activation = 'sigmoid', kernel_initializer=initializer))
+    model.add(Dense(32, activation = 'sigmoid', kernel_initializer=initializer))
+    model.add(Dense(nb_actions, activation = output_activation, kernel_initializer=initializer))
+    print(model.summary())
+    #opt = keras.optimizers.Adam(learning_rate=0.00001)
+    opt = keras.optimizers.Adam(learning_rate=lr)
+    model.compile(loss=tf.keras.losses.Huber(), optimizer=opt)
+    #model.compile(optimizer='adam', loss=tf.keras.losses.Huber())
+
+    return model
+'''
+def small_dense(input_len, output_activation, nb_actions, lr):
+    model = Sequential()
+    model.add(Dense(16, input_dim=input_len))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(32))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(nb_actions))
+    model.add(Activation(output_activation))
+    print(model.summary())
+    #opt = keras.optimizers.Adam(learning_rate=0.00001)
+    opt = keras.optimizers.Adam(learning_rate=lr)
+    model.compile(loss=tf.keras.losses.Huber(), optimizer=opt)
+    #model.compile(optimizer='adam', loss=tf.keras.losses.Huber())
+    return model
+'''
+def big_dense(input_len, output_activation, nb_actions, lr):
+    model = Sequential()
+    model.add(Dense(16, input_dim=input_len))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(32))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(64))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(128))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(256))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(512))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(256))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(128))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(64))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(32))
+    model.add(Activation('sigmoid'))
+    model.add(Dense(nb_actions))
+    model.add(Activation(output_activation))
+    print(model.summary())
+    opt = keras.optimizers.Adam(learning_rate=lr)
+    model.compile(loss=tf.keras.losses.Huber(), optimizer=opt)
+    #model.compile(optimizer='adam', loss=tf.keras.losses.Huber())
+
+    return model
+
+def mellowmax(omega, x):
+    sum_ = sum((math.exp(omega * val) for val in x))
+    return math.log(sum_/len(x))/omega
+
+def huber_loss(y_true, y_pred, clip_delta=1.0):
+    error = y_true - y_pred
+    cond  = tf.keras.backend.abs(error) < clip_delta
+
+    squared_loss = 0.5 * tf.keras.backend.square(error)
+    linear_loss  = clip_delta * (tf.keras.backend.abs(error) - 0.5 * clip_delta)
+
+    return tf.where(cond, squared_loss, linear_loss)
+
+def huber_loss_mean(y_true, y_pred, clip_delta=1.0):
+    return tf.keras.backend.mean(huber_loss(y_true, y_pred, clip_delta))
